98739228754e05234d34dfdd16f0a3888308c585
Jake Hamilton <hamiltonjm12@gmail.com>
2016-11-03 13:15:53 -0400

    Build cryptocompare DAO

---
+++ app/services/cryptocompare_dao.js
@@ -0,0 +1,54 @@
+const daemon = require('superagent');
+
+class CryptoCompare {
+  static allCoins () {
+    return daemon.get("https://www.cryptocompare.com/api/data/coinlist/")
+                 .then((response) => {
+                    const coins = response.data;
+                    const coinModels = [];
+                    for (let c of coins) {
+                      const clean = { name: c.FullName, };
+                      const coin = new Coin(clean);
+                      coinModels.push(coin);
+                    }
+                    return coinModels;
+                 });
+  }
+
+  static spotData (coinFrom, coinTo) {
+    return daemon.get(`https://www.cryptocompare.com/api/data/price?fsym=${coinFrom}&tsyms=${coinTo}`)
+                 .then((response) => {
+                    const cleanData = {
+                      price: response.data[0].price,
+                      volume24hFrom: response.data[0].Volume24Hours,
+                      volume24hTo: response.data[0].VolumeTo24HoursTo,
+                    };
+                    return cleanData;
+                 });
+  }
+
+  static getHistory (unit, length, interval, coinFrom, coinTo, exchange) {
+    const baseURL = `https://www.cryptocompare.com/api/data/histo${unit}/?`;
+    const aggregate = interval !== 1 ? `&aggregate=${interval}` : '';
+    const e = !exchange ? 'e=CCCAGG' : `e=${exchange}`;
+    return daemon.get(`${baseURL}${e}&fsym=${coinFrom}&tsym=${coinTo}&limit=${length}${aggregate}`)
+                 .then((response) => {
+                    const data = response.data;
+                    const clean = data.map((snapshot) => {
+                      const cleanSnapshot = {
+                        closingPrice: snapshot.close,
+                        volumeFrom: snapshot.volumefrom,
+                        volumeTo: snapshot.volumeto,
+                      };
+                      return cleanSnapshot;
+                    });
+                    const cleanData = {
+                      hasFirstValue: response.FirstValueInArray,
+                      data: clean,
+                    };
+                    return cleanData;
+                 })
+  }
+}
+
+modules.export = CryptoCompare;
